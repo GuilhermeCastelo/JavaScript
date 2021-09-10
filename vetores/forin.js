1c2e23f991009c0faa21852c74151a079d3c1afc
Guilherme <guilherme.senacastelo@gmail.com>
2021-09-10 12:28:39 -0300

    vetores

---
+++ vetores/forin.js
@@ -0,0 +1,6 @@
+let num = [5, 2, 6, 9];
+
+// para cada posição em num...
+for(let pos in num){
+    console.log("A posição " + pos + " tem o valor " + num[pos]);
+}
---
+++ vetores/vetor.js
@@ -0,0 +1,10 @@
+let num = [5, 2, 6, 9];
+
+//console.log(`O vetor tem o tamanho ${num.length}`);
+//console.log(`O vetor em ordem crescente fica ${num.sort()}`);
+
+for(let pos = 0; pos < num.length; pos++){
+    console.log("A posição " + pos + " tem o valor " + num[pos]);
+}
+
+console.log(num.indexOf(2));
