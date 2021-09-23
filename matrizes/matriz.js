1495fdebf7b64674030eabbe4e3f1e90507b5e23
Guilherme <guilherme.senacastelo@gmail.com>
2021-09-23 14:59:58 -0300

    iniciando matrizes

---
+++ matrizes/matriz.js
@@ -0,0 +1,11 @@
+let dados = [
+
+    pessoa = ["João", "123456789", "joao@gmail.com"],
+
+    pessoa = ["Maria", "987654321", "maria@gmail.com"],
+    
+    pessoa = ["Jorge", "123456987", "jorge@gmail.com"]
+
+];
+
+console.log(dados[0][1]);
